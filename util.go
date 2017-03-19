e84c34bb6ca3fc156f3722160d55e30166ffe7b4
Egon Elbre <egonelbre@gmail.com>
2017-03-19 11:29:54 +0200

    gofmt

--- util.go
+++ util.go
@@ -1,137 +1,137 @@
 // Package hdwallet implements heirarchical deterministic Bitcoin wallets, as defined in BIP 32.
-// 
+//
 // BIP 32 - https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
-// 
+//
 // This package provides utilities for generating hierarchical deterministic Bitcoin wallets.
-// 
+//
 // Examples
 //
 //          // Generate a random 256 bit seed
 //          seed, err := hdwallet.GenSeed(256)
-//          
+//
 //          // Create a master private key
 //          masterprv := hdwallet.MasterKey(seed)
-//          
+//
 //          // Convert a private key to public key
 //          masterpub := masterprv.Pub()
-//          
+//
 //          // Generate new child key based on private or public key
 //          childprv, err := masterprv.Child(0)
 //          childpub, err := masterpub.Child(0)
-//          
+//
 //          // Create bitcoin address from public key
 //          address := childpub.Address()
-//          
+//
 //          // Convenience string -> string Child and Address functions
 //          walletstring := childpub.String()
 //          childstring, err := hdwallet.StringChild(walletstring,0)
 //          childaddress, err := hdwallet.StringAddress(childstring)
-// 
+//
 // Extended Keys
-// 
+//
 // Hierarchical deterministic wallets are simply deserialized extended keys. Extended Keys can be imported and exported as base58-encoded strings. Here are two examples:
 //          public key:   "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8"
 //          private key:  "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi"
-// 
+//
 package hdwallet
 
 import (
-    "crypto/sha256"
-    "code.google.com/p/go.crypto/ripemd160"
-    "encoding/binary"
-    "encoding/hex"
-    "github.com/btcsuite/btcd/btcec"
-    "math/big"
-    )
+	"code.google.com/p/go.crypto/ripemd160"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"github.com/btcsuite/btcd/btcec"
+	"math/big"
+)
 
 var curve *btcec.KoblitzCurve = btcec.S256()
 
 func hash160(data []byte) []byte {
-    sha := sha256.New()
-    ripe := ripemd160.New()
-    sha.Write(data)
-    ripe.Write(sha.Sum(nil))
-    return ripe.Sum(nil)
+	sha := sha256.New()
+	ripe := ripemd160.New()
+	sha.Write(data)
+	ripe.Write(sha.Sum(nil))
+	return ripe.Sum(nil)
 }
 
 func dblSha256(data []byte) []byte {
-    sha1 := sha256.New()
-    sha2 := sha256.New()
-    sha1.Write(data)
-    sha2.Write(sha1.Sum(nil))
-    return sha2.Sum(nil)
+	sha1 := sha256.New()
+	sha2 := sha256.New()
+	sha1.Write(data)
+	sha2.Write(sha1.Sum(nil))
+	return sha2.Sum(nil)
 }
 
 func privToPub(key []byte) []byte {
-    return compress(curve.ScalarBaseMult(key))
+	return compress(curve.ScalarBaseMult(key))
 }
 
 func onCurve(x, y *big.Int) bool {
-    return curve.IsOnCurve(x,y)
+	return curve.IsOnCurve(x, y)
 }
 
 func compress(x, y *big.Int) []byte {
-    two := big.NewInt(2)
-    rem := two.Mod(y,two).Uint64()
-    rem += 2
-    b := make([]byte,2)
-    binary.BigEndian.PutUint16(b,uint16(rem))
-    rest := x.Bytes()
-    return append(b[1:],rest...)
+	two := big.NewInt(2)
+	rem := two.Mod(y, two).Uint64()
+	rem += 2
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, uint16(rem))
+	rest := x.Bytes()
+	return append(b[1:], rest...)
 }
 
 //2.3.4 of SEC1 - http://www.secg.org/index.php?action=secg,docs_secg
 func expand(key []byte) (*big.Int, *big.Int) {
-    params := curve.Params()
-    exp := big.NewInt(1)
-    exp.Add(params.P,exp)
-    exp.Div(exp,big.NewInt(4))
-    x := big.NewInt(0).SetBytes(key[1:33])
-    y := big.NewInt(0).SetBytes(key[:1])
-    beta := big.NewInt(0)
-    beta.Exp(x,big.NewInt(3),nil)
-    beta.Add(beta,big.NewInt(7))
-    beta.Exp(beta,exp,params.P)
-    if(y.Add(beta,y).Mod(y,big.NewInt(2)).Int64() == 0) {
-        y = beta
-    } else {
-        y = beta.Sub(params.P,beta)
-    }
-    return x,y
+	params := curve.Params()
+	exp := big.NewInt(1)
+	exp.Add(params.P, exp)
+	exp.Div(exp, big.NewInt(4))
+	x := big.NewInt(0).SetBytes(key[1:33])
+	y := big.NewInt(0).SetBytes(key[:1])
+	beta := big.NewInt(0)
+	beta.Exp(x, big.NewInt(3), nil)
+	beta.Add(beta, big.NewInt(7))
+	beta.Exp(beta, exp, params.P)
+	if y.Add(beta, y).Mod(y, big.NewInt(2)).Int64() == 0 {
+		y = beta
+	} else {
+		y = beta.Sub(params.P, beta)
+	}
+	return x, y
 }
 
 func addPrivKeys(k1, k2 []byte) []byte {
-    i1 := big.NewInt(0).SetBytes(k1)
-    i2 := big.NewInt(0).SetBytes(k2)
-    i1.Add(i1,i2)
-    i1.Mod(i1,curve.Params().N)
-    k := i1.Bytes()
-    zero,_ := hex.DecodeString("00")
-    return append(zero,k...)
+	i1 := big.NewInt(0).SetBytes(k1)
+	i2 := big.NewInt(0).SetBytes(k2)
+	i1.Add(i1, i2)
+	i1.Mod(i1, curve.Params().N)
+	k := i1.Bytes()
+	zero, _ := hex.DecodeString("00")
+	return append(zero, k...)
 }
 
 func addPubKeys(k1, k2 []byte) []byte {
-    x1,y1 := expand(k1)
-    x2,y2 := expand(k2)
-    return compress(curve.Add(x1,y1,x2,y2))
+	x1, y1 := expand(k1)
+	x2, y2 := expand(k2)
+	return compress(curve.Add(x1, y1, x2, y2))
 }
 
 func uint32ToByte(i uint32) []byte {
-    a := make([]byte, 4)
-    binary.BigEndian.PutUint32(a,i)
-    return a
+	a := make([]byte, 4)
+	binary.BigEndian.PutUint32(a, i)
+	return a
 }
 
 func uint16ToByte(i uint16) []byte {
-    a := make([]byte, 2)
-    binary.BigEndian.PutUint16(a,i)
-    return a[1:]
+	a := make([]byte, 2)
+	binary.BigEndian.PutUint16(a, i)
+	return a[1:]
 }
 
 func byteToUint16(b []byte) uint16 {
-    if len(b) == 1 {
-        zero := make([]byte,1)
-        b = append(zero,b...)
-    }
-    return binary.BigEndian.Uint16(b)
+	if len(b) == 1 {
+		zero := make([]byte, 1)
+		b = append(zero, b...)
+	}
+	return binary.BigEndian.Uint16(b)
 }
